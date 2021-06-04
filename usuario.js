0c33341776a2a4381c813686b0807ddf0e8a0d83
gustavolsj <gustavo.lozano.sa@usb.edu.mx>
2021-06-04 17:30:14 -0500

    usuario corregido

--- usuario.js
+++ usuario.js
@@ -3,9 +3,7 @@
 var p; // shortcut to reference prototypes
 var lib={};var ss={};var img={};
 lib.ssMetadata = [
-		{name:"usuario_atlas_1", frames: [[0,0,1080,1920]]},
-		{name:"usuario_atlas_2", frames: [[0,0,1080,1920]]},
-		{name:"usuario_atlas_3", frames: [[0,0,1080,1920]]}
+		{name:"usuario_atlas_1", frames: [[0,0,1080,1920]]}
 ];
 
 
@@ -42,94 +40,10 @@ p.nominalBounds = new cjs.Rectangle(0,0,1614,3868);
 
 
 (lib.config_usuariomdpi = function() {
-	this.initialize(ss["usuario_atlas_3"]);
-	this.gotoAndStop(0);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.registromdpi = function() {
-	this.initialize(ss["usuario_atlas_2"]);
-	this.gotoAndStop(0);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.splashmdpi = function() {
 	this.initialize(ss["usuario_atlas_1"]);
 	this.gotoAndStop(0);
 }).prototype = p = new cjs.Sprite();
 
-
-
-(lib.gr_splash = function(mode,startPosition,loop,reversed) {
-if (loop == null) { loop = true; }
-if (reversed == null) { reversed = false; }
-	var props = new Object();
-	props.mode = mode;
-	props.startPosition = startPosition;
-	props.labels = {};
-	props.loop = loop;
-	props.reversed = reversed;
-	cjs.MovieClip.apply(this,[props]);
-
-	// Layer_1
-	this.instance = new lib.splashmdpi();
-	this.instance.setTransform(-540,-960);
-
-	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
-
-	this._renderFirstFrame();
-
-}).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(-540,-960,1080,1920);
-
-
-(lib.gr_login = function(mode,startPosition,loop,reversed) {
-if (loop == null) { loop = true; }
-if (reversed == null) { reversed = false; }
-	var props = new Object();
-	props.mode = mode;
-	props.startPosition = startPosition;
-	props.labels = {};
-	props.loop = loop;
-	props.reversed = reversed;
-	cjs.MovieClip.apply(this,[props]);
-
-	// Layer_1
-	this.instance = new lib.registromdpi();
-	this.instance.setTransform(-540,-960);
-
-	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
-
-	this._renderFirstFrame();
-
-}).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(-540,-960,1080,1920);
-
-
-(lib.gr_letras = function(mode,startPosition,loop,reversed) {
-if (loop == null) { loop = true; }
-if (reversed == null) { reversed = false; }
-	var props = new Object();
-	props.mode = mode;
-	props.startPosition = startPosition;
-	props.labels = {};
-	props.loop = loop;
-	props.reversed = reversed;
-	cjs.MovieClip.apply(this,[props]);
-
-	// Layer_1
-	this.shape = new cjs.Shape();
-	this.shape.graphics.f("#FFFFFF").s().p("EhUXCWAMAAAkr/MCovAAAMAAAEr/g");
-	this.shape.setTransform(9.7421,-5.3516,0.2315,0.0365);
-
-	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
-
-	this._renderFirstFrame();
-
-}).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(-115.2,-40.3,250,70);
 
 
 (lib.gr_barra_gris = function(mode,startPosition,loop,reversed) {
@@ -230,19 +144,21 @@ if (reversed == null) { reversed = false; }
 p.nominalBounds = new cjs.Rectangle(0,0,1080,1920);
 
 
-(lib.mc_perfil = function(mode,startPosition,loop,reversed) {
-if (loop == null) { loop = true; }
+// stage content:
+(lib.test = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = false; }
 if (reversed == null) { reversed = false; }
 	var props = new Object();
 	props.mode = mode;
 	props.startPosition = startPosition;
-	props.labels = {menu:0,entrada:1,salida:21,usuario:44,maceta:66,ayuda:88};
+	props.labels = {menu:47,entrada:48,salida:68,usuario:91,maceta:113,ayuda:135};
 	props.loop = loop;
 	props.reversed = reversed;
 	cjs.MovieClip.apply(this,[props]);
 
+	this.actionFrames = [47,65,84,106,128,150];
 	// timeline functions:
-	this.frame_0 = function() {
+	this.frame_47 = function() {
 		var _detener = this;
 		_detener.stop();
 		
@@ -263,7 +179,7 @@ if (reversed == null) { reversed = false; }
 		_this.gotoAndPlay('entrada');
 		});
 	}
-	this.frame_18 = function() {
+	this.frame_65 = function() {
 		var _detener = this;
 		_detener.stop();
 		
@@ -291,135 +207,78 @@ if (reversed == null) { reversed = false; }
 		_ayuda.gotoAndPlay('ayuda');
 		});
 	}
-	this.frame_37 = function() {
+	this.frame_84 = function() {
 		var _this = this;
 		_this.gotoAndStop('menu');
 	}
-	this.frame_59 = function() {
+	this.frame_106 = function() {
 		var _this = this;
 		window.open('usuario.html', '_self');
 	}
-	this.frame_81 = function() {
+	this.frame_128 = function() {
 		var _this = this;
 		window.open('maceta.html', '_self');
 	}
-	this.frame_103 = function() {
+	this.frame_150 = function() {
 		var _this = this;
 		window.open('ayuda.html', '_self');
 	}
 
 	// actions tween:
-	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(18).call(this.frame_18).wait(19).call(this.frame_37).wait(22).call(this.frame_59).wait(22).call(this.frame_81).wait(22).call(this.frame_103).wait(2));
+	this.timeline.addTween(cjs.Tween.get(this).wait(47).call(this.frame_47).wait(18).call(this.frame_65).wait(19).call(this.frame_84).wait(22).call(this.frame_106).wait(22).call(this.frame_128).wait(22).call(this.frame_150).wait(23));
 
 	// interactivo
 	this.btn_hamburguesa = new lib.btn_null();
 	this.btn_hamburguesa.name = "btn_hamburguesa";
-	this.btn_hamburguesa.setTransform(416.05,-814.5);
+	this.btn_hamburguesa.setTransform(956.05,145.5);
 	new cjs.ButtonHelper(this.btn_hamburguesa, 0, 1, 2, false, new lib.btn_null(), 3);
 
 	this.btn_ayuda = new lib.btn_null();
 	this.btn_ayuda.name = "btn_ayuda";
-	this.btn_ayuda.setTransform(-181.85,-123.85,4.1783,1);
+	this.btn_ayuda.setTransform(358.15,836.15,4.1783,1);
 	new cjs.ButtonHelper(this.btn_ayuda, 0, 1, 2, false, new lib.btn_null(), 3);
 
 	this.btn_maceta = new lib.btn_null();
 	this.btn_maceta.name = "btn_maceta";
-	this.btn_maceta.setTransform(-181.85,-526.15,4.1783,1);
+	this.btn_maceta.setTransform(358.15,433.85,4.1783,1);
 	new cjs.ButtonHelper(this.btn_maceta, 0, 1, 2, false, new lib.btn_null(), 3);
 
 	this.btn_usuario = new lib.btn_null();
 	this.btn_usuario.name = "btn_usuario";
-	this.btn_usuario.setTransform(-181.85,-686.2,4.1783,1);
+	this.btn_usuario.setTransform(358.15,273.8,4.1783,1);
 	new cjs.ButtonHelper(this.btn_usuario, 0, 1, 2, false, new lib.btn_null(), 3);
 
-	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.btn_hamburguesa}]}).to({state:[{t:this.btn_hamburguesa},{t:this.btn_usuario},{t:this.btn_maceta},{t:this.btn_ayuda}]},17).to({state:[]},20).to({state:[{t:this.btn_hamburguesa},{t:this.btn_usuario}]},7).to({state:[]},16).to({state:[{t:this.btn_hamburguesa},{t:this.btn_usuario}]},6).to({state:[]},16).to({state:[{t:this.btn_hamburguesa},{t:this.btn_usuario}]},6).to({state:[]},16).wait(1));
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[]}).to({state:[{t:this.btn_hamburguesa}]},47).to({state:[{t:this.btn_hamburguesa},{t:this.btn_usuario},{t:this.btn_maceta},{t:this.btn_ayuda}]},17).to({state:[]},20).to({state:[{t:this.btn_hamburguesa},{t:this.btn_usuario}]},7).to({state:[]},16).to({state:[{t:this.btn_hamburguesa},{t:this.btn_usuario}]},6).to({state:[]},16).to({state:[{t:this.btn_hamburguesa},{t:this.btn_usuario}]},6).to({state:[]},17).wait(21));
 
 	// Barra_menu
 	this.instance = new lib.gr_barra("synched",0);
-	this.instance.setTransform(-940,0);
+	this.instance.setTransform(-400,960);
 	this.instance._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1).to({_off:false},0).to({x:-140},15).wait(5).to({startPosition:0},0).to({x:-940},15).to({_off:true},1).wait(7).to({_off:false,x:-140},0).to({x:-940},15).to({_off:true},1).wait(6).to({_off:false,x:-140},0).to({x:-940},15).to({_off:true},1).wait(6).to({_off:false,x:-140},0).to({x:-940},15).to({_off:true},1).wait(1));
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(48).to({_off:false},0).to({x:400},15).wait(5).to({startPosition:0},0).to({x:-400},15).to({_off:true},1).wait(7).to({_off:false,x:400},0).to({x:-400},15).to({_off:true},1).wait(6).to({_off:false,x:400},0).to({x:-400},15).to({_off:true},1).wait(6).to({_off:false,x:400},0).to({x:-400},15).to({_off:true},2).wait(21));
 
 	// Barra_gris
 	this.instance_1 = new lib.gr_gris("synched",0);
-	this.instance_1.setTransform(0,0,1,1,0,0,0,540,960);
+	this.instance_1.setTransform(540,960,1,1,0,0,0,540,960);
 	this.instance_1.alpha = 0;
 	this.instance_1._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(1).to({_off:false},0).to({alpha:0.3906},15).wait(5).to({startPosition:0},0).to({alpha:0},15).to({_off:true},1).wait(7).to({_off:false,alpha:0.3906},0).to({alpha:0},15).to({_off:true},1).wait(6).to({_off:false,alpha:0.3906},0).to({alpha:0},15).to({_off:true},1).wait(6).to({_off:false,alpha:0.3906},0).to({alpha:0},15).to({_off:true},1).wait(1));
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(48).to({_off:false},0).to({alpha:0.3906},15).wait(5).to({startPosition:0},0).to({alpha:0},15).to({_off:true},1).wait(7).to({_off:false,alpha:0.3906},0).to({alpha:0},15).to({_off:true},1).wait(6).to({_off:false,alpha:0.3906},0).to({alpha:0},15).to({_off:true},1).wait(6).to({_off:false,alpha:0.3906},0).to({alpha:0},15).to({_off:true},2).wait(21));
 
 	// usuario
 	this.instance_2 = new lib.config_usuariomdpi();
-	this.instance_2.setTransform(-540,-960);
 
 	this.btn_hamburguesa_1 = new lib.btn_null();
 	this.btn_hamburguesa_1.name = "btn_hamburguesa_1";
-	this.btn_hamburguesa_1.setTransform(416.05,-814.5);
+	this.btn_hamburguesa_1.setTransform(956.05,145.5);
 	new cjs.ButtonHelper(this.btn_hamburguesa_1, 0, 1, 2, false, new lib.btn_null(), 3);
 
-	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.btn_hamburguesa_1},{t:this.instance_2}]}).to({state:[{t:this.btn_hamburguesa_1},{t:this.instance_2}]},104).wait(1));
-
-	this._renderFirstFrame();
-
-}).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(-1340,-967,1880,1934);
-
-
-// stage content:
-(lib.Plantas = function(mode,startPosition,loop,reversed) {
-if (loop == null) { loop = false; }
-if (reversed == null) { reversed = false; }
-	var props = new Object();
-	props.mode = mode;
-	props.startPosition = startPosition;
-	props.labels = {};
-	props.loop = loop;
-	props.reversed = reversed;
-	cjs.MovieClip.apply(this,[props]);
-
-	// rect2
-	this.gr_rect2 = new lib.gr_letras("synched",0);
-	this.gr_rect2.name = "gr_rect2";
-	this.gr_rect2.setTransform(347.9,1199.05);
-	this.gr_rect2._off = true;
-
-	this.timeline.addTween(cjs.Tween.get(this.gr_rect2).wait(38).to({_off:false},0).wait(14).to({startPosition:0},0).to({x:560.55,y:1189.3},15).to({_off:true},1).wait(1));
-
-	// rect1
-	this.instance = new lib.gr_letras("synched",0);
-	this.instance.setTransform(340.1,1066.55);
-	this.instance._off = true;
-
-	this.gr_rect1 = new lib.gr_letras("synched",0);
-	this.gr_rect1.name = "gr_rect1";
-	this.gr_rect1.setTransform(560.55,1066.55);
-
-	this.timeline.addTween(cjs.Tween.get({}).to({state:[]}).to({state:[{t:this.instance}]},38).to({state:[{t:this.gr_rect1}]},14).to({state:[]},16).wait(1));
-	this.timeline.addTween(cjs.Tween.get(this.instance).wait(38).to({_off:false},0).to({_off:true,x:560.55},14).wait(17));
-
-	// menu_hamburguesa
-	this.instance_1 = new lib.mc_perfil();
-	this.instance_1.setTransform(540,960);
-	this.instance_1._off = true;
-
-	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(68).to({_off:false},0).wait(1));
-
-	// Splash_login
-	this.instance_2 = new lib.gr_splash("synched",0);
-	this.instance_2.setTransform(539.95,960);
-	this.instance_2.alpha = 0;
-
-	this.instance_3 = new lib.gr_login("synched",0);
-	this.instance_3.setTransform(540,960);
-
-	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_2}]}).to({state:[{t:this.instance_2}]},18).to({state:[{t:this.instance_2}]},1).to({state:[{t:this.instance_2}]},18).to({state:[{t:this.instance_2},{t:this.instance_3}]},1).to({state:[{t:this.instance_2},{t:this.instance_3}]},29).to({state:[]},1).wait(1));
-	this.timeline.addTween(cjs.Tween.get(this.instance_2).to({x:540,alpha:1},18,cjs.Ease.quadOut).wait(1).to({startPosition:0},0).to({x:539.95,alpha:0},18).wait(1).to({startPosition:0},0).wait(29).to({startPosition:0},0).to({_off:true},1).wait(1));
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[]}).to({state:[{t:this.btn_hamburguesa_1},{t:this.instance_2}]},47).to({state:[{t:this.btn_hamburguesa_1},{t:this.instance_2}]},104).to({state:[]},1).wait(21));
 
 	this._renderFirstFrame();
 
 }).prototype = p = new lib.AnMovieClip();
-p.nominalBounds = new cjs.Rectangle(540,960,540,960);
+p.nominalBounds = new cjs.Rectangle(-260,0,1340,1927);
 // library properties:
 lib.properties = {
 	id: 'E2B250C286D598489CAF6B8841BE14B8',
@@ -429,11 +288,9 @@ lib.properties = {
 	color: "#FFFFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/CachedBmp_2.png?1622833871870", id:"CachedBmp_2"},
-		{src:"images/CachedBmp_1.png?1622833871870", id:"CachedBmp_1"},
-		{src:"images/usuario_atlas_1.png?1622833871833", id:"usuario_atlas_1"},
-		{src:"images/usuario_atlas_2.png?1622833871833", id:"usuario_atlas_2"},
-		{src:"images/usuario_atlas_3.png?1622833871834", id:"usuario_atlas_3"}
+		{src:"images/CachedBmp_2.png", id:"CachedBmp_2"},
+		{src:"images/CachedBmp_1.png", id:"CachedBmp_1"},
+		{src:"images/usuario_atlas_1.png", id:"usuario_atlas_1"}
 	],
 	preloads: []
 };
